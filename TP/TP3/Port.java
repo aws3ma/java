08d9b409cd687c521f9e3e7a5a3a6e243e3972bb
Oussema Trabelsi <79041057+aws3ma@users.noreply.github.com>
2022-04-09 11:20:11 +0100

    add ajouterbarque different method

--- TP/TP3/Port.java
+++ TP/TP3/Port.java
@@ -8,6 +8,7 @@ public class Port {
     private float taxe;
     private int nbBarque;
     private ArrayList<Barque> listBarque;
+
     public Port(String nom, int capacite, float taxe) {
         this.nom = nom;
         this.capacite = capacite;
@@ -15,41 +16,66 @@ public class Port {
         this.nbBarque = 0;
         this.listBarque = new ArrayList<>();
     }
+
     public void ajouterBarque(Barque b) {
-        if(nbBarque<capacite)
-        {listBarque.add(b);
-        nbBarque++;}else{
+        if (nbBarque < capacite) {
+            listBarque.add(b);
+            nbBarque++;
+        } else {
             System.out.println("Port est plein");
         }
     }
-    public void suppBarque(Barque b){
-        if(listBarque.remove(b)){
+
+    public void ajouterBarque(String num, String nom) {
+        if (nbBarque < capacite) {
+            Barque b = new Barque(nom, num);
+            listBarque.add(b);
+            nbBarque++;
+        } else {
+            System.out.println("Port est plein");
+        }
+    }
+
+    public void ajouterBarque(String num, String nom, int p) {
+        if (nbBarque < capacite) {
+            Barque b = new BarqueMoteur(nom, num, p);
+            listBarque.add(b);
+            nbBarque++;
+        } else {
+            System.out.println("Port est plein");
+        }
+    }
+
+    public void suppBarque(Barque b) {
+        if (listBarque.remove(b)) {
             System.out.println("Barque est quitter");
             nbBarque--;
-        }
-        else{
+        } else {
             System.out.println("Barque n'est pas disponible");
         }
     }
-    public float calculerTaxe(Barque b){
-        if(b instanceof BarqueMoteur){
-            if(((BarqueMoteur)b).getPuissance()<5)
-                return taxe*1.1f;
+
+    public float calculerTaxe(Barque b) {
+        if (b instanceof BarqueMoteur) {
+            if (((BarqueMoteur) b).getPuissance() < 5)
+                return taxe * 1.1f;
             else
-                return taxe*1.2f;
-        }else{
+                return taxe * 1.2f;
+        } else {
             return taxe;
         }
     }
-    public float recetteTaxeAujour(){
-        float som=0;
+
+    public float recetteTaxeAujour() {
+        float som = 0;
         for (Barque barque : listBarque) {
-            som+=calculerTaxe(barque);
+            som += calculerTaxe(barque);
         }
         return som;
     }
+
     @Override
     public String toString() {
-        return "Nom : "+nom+"\nCapacite : "+capacite+"\nTotal taxe : "+recetteTaxeAujour();
+        return "Nom : " + nom + "\nCapacite : " + capacite + "\nTotal taxe : " + recetteTaxeAujour();
     }
 }
